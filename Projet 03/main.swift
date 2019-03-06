4be7471993abaa4d0b8b86d23e370cdc3d207100
Massinissa75 <massinissa1theking@Gmail.com>
2019-03-06 16:56:34 +0100

    Initial Commit

---
+++ Projet 03/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet 03
+//
+//  Created by Massinissa_theking on 06/03/2019.
+//  Copyright © 2019 Massinissa_theking. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
